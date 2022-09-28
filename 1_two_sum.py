5c10010afb6fd7013d154a8c432ddc43381b97de
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-09-28 14:41:14 +0200

    pushed challenges 1

---
+++ 1_two_sum.py
@@ -0,0 +1,19 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+#ACCEPTED
+def two_sum(target, nums):
+    for i in nums:
+        for j in range(nums.index(i) + 1, len(nums)):
+            if i + nums[j] == target:
+                return [nums.index(i), j]
+
+nums = [2,5,5,11]
+target = int(input("integer target: "))
+print(two_sum(target, nums))
+
