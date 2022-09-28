00bffb6ceb71f33a4a75926a156d360fab17542c
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-09-28 15:28:02 +0200

    did task 3

---
+++ 3_longest_sub.py
@@ -0,0 +1,22 @@
+'''
+Given a string s, find the length of the longest substring without repeating characters.
+'''
+
+#SUCCESS
+def lengthOfLongestSubstring(s: str) -> int:
+    result = 0
+    for i in range(len(s)):
+        temp = str(s[i])
+        for j in range(i + 1, len(s)):
+            if s[j] not in temp:
+                temp += s[j]
+            else:
+                break
+        if len(temp) > result:
+            result = len(temp)
+    return result
+
+test = "abcabcbb"
+print(lengthOfLongestSubstring(test))
+
+
